822a537861f4506c51123a7928f70d26e4d2e92f
S Suwannapaond <saharat.rus@gmail.com>
2025-03-19 11:44:49 +0700

    update

--- cmd/main.go
+++ cmd/main.go
@@ -1,22 +1,27 @@
 package main
 
 import (
-	"github.com/Sarus1997/golang-api/config"
 	"github.com/Sarus1997/golang-api/internal/handlers"
-
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	config.LoadEnv()
-
+	//* สร้าง Gin router
 	r := gin.Default()
 
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "N O O B",
+		})
+	})
+
+	//* routes สำหรับ authentication
 	authGroup := r.Group("/auth")
 	{
 		authGroup.POST("/register", handlers.Register)
 		authGroup.POST("/login", handlers.Login)
 	}
 
+	//* port 8080
 	r.Run(":8080")
 }
--- config/config.go
+++ config/config.go
@@ -1,23 +1,40 @@
 package config
 
 import (
+	"fmt"
 	"log"
 	"os"
 
 	"github.com/joho/godotenv"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
-func LoadEnv() {
+var DB *gorm.DB
+
+// โหลด .env และเชื่อมต่อฐานข้อมูล
+func InitDatabase() {
+	// โหลดไฟล์ .env
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
-}
 
-func GetEnv(key string, defaultValue string) string {
-	value, exists := os.LookupEnv(key)
-	if !exists {
-		return defaultValue
+	// รับค่าจาก .env
+	dbHost := os.Getenv("DB_HOST")
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
+
+	// สร้าง DSN (Data Source Name) สำหรับ MySQL
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:3306)/%s?charset=utf8&parseTime=True&loc=Local", dbUser, dbPassword, dbHost, dbName)
+
+	// เชื่อมต่อฐานข้อมูล MySQL
+	database, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("failed to connect to MySQL", err)
 	}
-	return value
+
+	// เก็บ connection ไว้ในตัวแปร global
+	DB = database
 }
